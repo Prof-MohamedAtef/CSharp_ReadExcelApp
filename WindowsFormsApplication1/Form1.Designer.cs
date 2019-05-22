332eb4e28c5def5d15995feba0c4e614a52dfc36
Prof-MohamedAtef <proofesser@gmail.com>
2019-05-22 04:28:39 -0700

    reading excel with CSharp

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ExcelWBToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accessWithTableNameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.accessDBFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.excelToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.accessToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(415, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.exportToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem,
+            this.ExcelWBToolStripMenuItem,
+            this.accessWithTableNameToolStripMenuItem,
+            this.accessDBFileToolStripMenuItem});
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newToolStripMenuItem.Text = "Import";
+            // 
+            // excelToolStripMenuItem
+            // 
+            this.excelToolStripMenuItem.Name = "excelToolStripMenuItem";
+            this.excelToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.excelToolStripMenuItem.Text = "Excel With Sheet Name";
+            this.excelToolStripMenuItem.Click += new System.EventHandler(this.excelToolStripMenuItem_Click);
+            // 
+            // ExcelWBToolStripMenuItem
+            // 
+            this.ExcelWBToolStripMenuItem.Name = "ExcelWBToolStripMenuItem";
+            this.ExcelWBToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.ExcelWBToolStripMenuItem.Text = "Excel Workbook";
+            this.ExcelWBToolStripMenuItem.Click += new System.EventHandler(this.ExcelWBToolStripMenuItem_Click);
+            // 
+            // accessWithTableNameToolStripMenuItem
+            // 
+            this.accessWithTableNameToolStripMenuItem.Name = "accessWithTableNameToolStripMenuItem";
+            this.accessWithTableNameToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.accessWithTableNameToolStripMenuItem.Text = "Access with Table Name";
+            // 
+            // accessDBFileToolStripMenuItem
+            // 
+            this.accessDBFileToolStripMenuItem.Name = "accessDBFileToolStripMenuItem";
+            this.accessDBFileToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.accessDBFileToolStripMenuItem.Text = "Access D.B File";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.excelToolStripMenuItem1,
+            this.accessToolStripMenuItem1});
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // excelToolStripMenuItem1
+            // 
+            this.excelToolStripMenuItem1.Name = "excelToolStripMenuItem1";
+            this.excelToolStripMenuItem1.Size = new System.Drawing.Size(110, 22);
+            this.excelToolStripMenuItem1.Text = "Excel";
+            // 
+            // accessToolStripMenuItem1
+            // 
+            this.accessToolStripMenuItem1.Name = "accessToolStripMenuItem1";
+            this.accessToolStripMenuItem1.Size = new System.Drawing.Size(110, 22);
+            this.accessToolStripMenuItem1.Text = "Access";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(137, 17);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(248, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(370, 147);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(399, 199);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Insert Sheet Name :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(415, 239);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ExcelWBToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem excelToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem accessToolStripMenuItem1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStripMenuItem accessWithTableNameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem accessDBFileToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Data.OleDb;
+using System.Reflection;
+using Microsoft.Office.Interop;
+using Excel = Microsoft.Office.Interop.Excel; // Add Reference : COM Tab : Microsoft Excel Object Library ...
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void excelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataSet1.Clear();
+            if (textBox1.Text==string.Empty)
+            {
+                MessageBox.Show("Insert Sheet Name In the Text Box");   
+            }
+            else
+            {
+                string strsheet = textBox1.Text;
+                OpenFileDialog openfiledialouge1 = new OpenFileDialog();
+                openfiledialouge1.Filter = "Spread Sheets(*.*)|*.xlsx|All Files (*.*)|*.*";
+
+                if (openfiledialouge1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    try
+                    {
+                        string strfilename = openfiledialouge1.FileName;
+                        OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + strfilename + ";Extended Properties=Excel 12.0");
+                        OleDbDataAdapter adapter = new OleDbDataAdapter("select * from [TimeTable$]", con);//["+strsheet.ToString()+"$]", con);
+                        adapter.Fill(dataSet1);
+                        dataGridView1.DataSource = dataSet1.Tables[0];
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+            }
+        }
+
+        private void ExcelWBToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataSet1.Clear();
+            string strsheet = textBox1.Text;
+            OpenFileDialog openfiledialouge1 = new OpenFileDialog();
+            openfiledialouge1.Filter = "Spread Sheets(*.*)|*.xlsx|All Files (*.*)|*.*";
+
+            if (openfiledialouge1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    string strfilename = openfiledialouge1.FileName;
+                    OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + strfilename + ";Extended Properties=Excel 12.0");
+                    OleDbDataAdapter adapter = new OleDbDataAdapter("select * from [TimeTable$]", con);//["+strsheet.ToString()+"$]", con);
+                    adapter.Fill(dataSet1);
+                    dataGridView1.DataSource = dataSet1.Tables[0];
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
